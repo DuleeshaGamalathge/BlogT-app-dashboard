02fccbea49fe427e63ee6dd480e6d4304c2a1cf6
Duleesha Gamalathge <duleeshagamalathge@gmail.com>
2023-12-01 18:28:56 +0530

    setup firebase into the project and create categories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+// import {environment} from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment.prod';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    // AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+// environment.prod.ts
+export const environment = {
+    production: true,
+    // apiUrl: 'https://production-api.com/api',
+    // Other production-specific configurations
+    firebaseConfig: {
+        apiKey: "AIzaSyAmAz0MaVfP-JYLXMRFG0h1PPf8b8KXk9w",
+        authDomain: "blogt-3e820.firebaseapp.com",
+        projectId: "blogt-3e820",
+        storageBucket: "blogt-3e820.appspot.com",
+        messagingSenderId: "1097163622025",
+        appId: "1:1097163622025:web:ad9a2ca77882689d2e2858"
+      }
+  };
+  
